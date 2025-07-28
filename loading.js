0e5845cf10619fea7b59c9d5ce16afaea9089b52
jinjahongo <jjhcre@gmail.com>
2025-07-28 16:05:52 +0900

    Update loading.js

--- loading.js
+++ loading.js
@@ -1,6 +1,10 @@
 // loading.js
 document.addEventListener('DOMContentLoaded', () => {
   setTimeout(() => {
+    // 로딩 화면 숨김
     document.getElementById('loader').classList.add('hide');
+
+    // ✅ GNB 등장 애니메이션 트리거
+    document.querySelector('.gnb').classList.add('show');
   }, 2500);
 });
